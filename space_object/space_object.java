d4dbcba23cfcbe20383f5433178e4ed560d1b615
lbh0006 <lbh0006@uah.edu>
2016-02-01 18:43:22 -0600

    with magnitude

---
+++ space_object/space_object.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SkyMap;
+
+/**
+ *
+ * @author Lindsey Harris
+ */
+public class space_object {
+    float magnitude;
+    // To be continued...
+}
+
