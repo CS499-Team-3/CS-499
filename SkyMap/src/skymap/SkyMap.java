b27fd8bc092a8f31a779b9dfe33bfe3b08b74237
Lindsey Harris <lbh0006@uah.edu>
2016-03-19 13:07:59 -0500

    Adding pictures!

--- SkyMap/src/skymap/SkyMap.java
+++ SkyMap/src/skymap/SkyMap.java
@@ -17,33 +17,7 @@ public class SkyMap {
      */
     public static void main(String[] args) {
 
-//        //GUIWindow window = new GUIWindow();
-//        GUIWindow window = new GUIWindow();
-//        //Lindsey Tests
-//        Planet p = new Planet("sun");
-//        AstronomyCalculator c = new AstronomyCalculator();
-//        c.usePrecalculatedPlanetElems(p, 2448000.5); // 2415128.5
-//        //testLindseysFunctions(p,2448000.5);
-
-
-        // TODO code application logic here
-
-        //testing the SpaceFactory class
-
-        //SpaceFactory factory = new SpaceFactory();
-
-        //SpaceObject x = factory.makeSpaceObject("planet");
-
-        Planet Mercury = new Planet("MERCURY");
-        Planet Venus = new Planet("VENUS");
-        Planet Earth = new Planet("EARTH");
-        Planet Mars = new Planet("MARS");
-        Planet Jupiter = new Planet("JUPITER");
-        Planet Saturn = new Planet("SATURN");
-        Planet Uranus = new Planet("URANUS");
-        Planet Neptune = new Planet("NEPTUNE");
-        testPlanetElementFunctions(Mercury);
-
+        GUIWindow window = new GUIWindow();
 
     }
 
@@ -70,20 +44,20 @@ public class SkyMap {
 
     }
     
-//    public static void testLindseysFunctions(Planet planet, double julianDate)
-//    {
-//        AstronomyCalculator calc = new AstronomyCalculator();
-//        Coordinate coord = calc.getRectEquatCoord(planet, julianDate);
-//        double dec = calc.getDeclination(planet, julianDate);
-//        double rightAscen = calc.getRightAscension(planet, julianDate);
-//
-//        System.out.println("Planet: "+planet.name);
-//        System.out.println("JulianDate: "+julianDate);
-//        System.out.println("Coordinates:");
-//        System.out.println(  "     x: "+coord.x+
-//                           "\n     y: "+coord.y+
-//                           "\n     z: "+coord.z);
-//        System.out.println("declination: "+dec);
-//        System.out.println("right ascension: "+rightAscen);
-//    }
+    public static void testLindseysFunctions(Planet planet, double julianDate)
+    {
+        AstronomyCalculator calc = new AstronomyCalculator();
+        Coordinate coord = calc.getRectEquatCoord(planet, julianDate);
+        double dec = calc.getDeclination(planet, julianDate);
+        double rightAscen = calc.getRightAscension(planet, julianDate);
+
+        System.out.println("Planet: "+planet.name);
+        System.out.println("JulianDate: "+julianDate);
+        System.out.println("Coordinates:");
+        System.out.println(  "     x: "+coord.x+
+                           "\n     y: "+coord.y+
+                           "\n     z: "+coord.z);
+        System.out.println("declination: "+dec);
+        System.out.println("right ascension: "+rightAscen);
+    }
 }
