1a0d509ec170e86960207b03cb46babd9d828863
Joseph Noyes <joseph.noyes@gmail.com>
2016-04-17 21:06:56 -0500

    Committed a better main

--- SkyMap/src/skymap/SkyMap.java
+++ SkyMap/src/skymap/SkyMap.java
@@ -17,64 +17,11 @@ public class SkyMap {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AstronomyCalculator calc = new AstronomyCalculator();
-        double julianDate = calc.calRelativeJulian(2009,1,15,5,30,0);
+        
+        //read embeded csv file, call then calls GUI Window to start program
         DataParser parser = DataParser.getParser();
         parser.readFile();
-        
-        SkyBox skyBox = SkyBox.getSkyBox();
-        List<Planet> planetList = skyBox.getPlanetList();
-        List<Star> starList = skyBox.getStarList();
-        
-        for(int i = 0; i < planetList.size(); i++)
-        {
-            calc.usePrecalculatedPlanetElems(planetList.get(i), julianDate);
-            planetList.get(i).location = calc.getPlanetPos(skyBox.lon, skyBox.lat, planetList.get(i), julianDate);  
-        }
-        for(int i = 0; i < starList.size(); i++)
-        {
-            starList.get(i).location = 
-                    calc.getPosition(skyBox.lon, skyBox.lat, starList.get(i), julianDate);
-        }
         GUIWindow window = new GUIWindow();
     }
 
-
-    public static void testPlanetElementFunctions(Planet planet)
-    {
-        AstronomyCalculator calc = new AstronomyCalculator();
-
-        double d2d = calc.date2Decimal(2016, 3, 11);
-        System.out.println("Date2Decimal: " + d2d);
-        double reljd = calc.calRelativeJulian(1997, 8, 8, 16, 4, 0);
-        System.out.println("JulianDate: " + reljd);
-        System.out.println(planet.name);
-        calc.usePrecalculatedPlanetElems(planet, reljd);
-        System.out.println("Mean Longitude: " + planet.mean_longitude);
-        System.out.println("Semi-Major Axis: " + planet.semiMajor_axis);
-        System.out.println("Eccentricity: " + planet.eccentricity);
-        System.out.println("Inclination: " + planet.inclination);
-        System.out.println("Perihelion: " + planet.perihelion);
-        System.out.println("Longitude Ascending: " + planet.longitude_ascending);
-
-    }
-
-
-    
-    public static void testLindseysFunctions(Planet planet, double julianDate)
-    {
-        AstronomyCalculator calc = new AstronomyCalculator();
-        Coordinate coord = calc.getRectEquatCoord(planet, julianDate);
-        double dec = calc.getDeclination(planet, julianDate);
-        double rightAscen = calc.getRightAscension(planet, julianDate);
-
-        System.out.println("Planet: "+planet.name);
-        System.out.println("JulianDate: "+julianDate);
-        System.out.println("Coordinates:");
-        System.out.println(  "     x: "+coord.x+
-                           "\n     y: "+coord.y+
-                           "\n     z: "+coord.z);
-        System.out.println("declination: "+dec);
-        System.out.println("right ascension: "+rightAscen);
-    }
 }
